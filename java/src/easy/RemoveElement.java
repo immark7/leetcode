5ff36b785367eb6a5c74b087c03659a54e4594ce
immark7 <immarkcuoi@gmail.com>
2023-11-29 09:46:03 +0700

    solve: remove element

---
+++ java/src/easy/RemoveElement.java
@@ -0,0 +1,25 @@
+package easy;
+
+//Problem: https://leetcode.com/problems/remove-element/submissions/
+class Solution {
+    public static void main(String[] args) {
+        int[] nums = {3, 2, 2, 3};
+        int val = 3;
+        int length = removeElement(nums, val);
+        System.out.println("Result: " + length);
+    }
+
+    public static int removeElement(int[] nums, int val) {
+        int n = nums.length;
+        int count = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (nums[i] != val) {
+                nums[count] = nums[i];
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
