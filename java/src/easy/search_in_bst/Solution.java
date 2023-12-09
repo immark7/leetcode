624605be650b9f4d0faa8ba43556512136da93b7
immark7 <immarkcuoi@gmail.com>
2023-12-09 18:33:47 +0700

    solve: search in a bst

---
+++ java/src/easy/search_in_bst/Solution.java
@@ -0,0 +1,17 @@
+package easy.search_in_bst;
+
+class Solution {
+    public TreeNode searchBST(TreeNode root, int val) {
+        if (root == null) {
+            return null;
+        }
+
+        if (val > root.val) {
+            return searchBST(root.right, val);
+        } else if (val < root.val) {
+            return searchBST(root.left, val);
+        } else {
+            return root;
+        }
+    }
+}
