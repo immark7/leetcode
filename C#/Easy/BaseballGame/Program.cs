3576daaa6d9deec1cb140e4a505601f1f4b220cf
immark7 <immarkcuoi@gmail.com>
2024-01-16 23:28:48 +0700

    feat: solve baseball game

---
+++ C#/Easy/BaseballGame/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace BaseballGame
+{
+    public class Program
+    {
+        public static void Main()
+        {   
+            Game baseballGame = new Game();
+            string[] input = { "5", "2", "C", "D", "+" };
+            int result = baseballGame.CalPoints(input);
+            Console.WriteLine("Result: " + result);
+        }
+    }
+}
