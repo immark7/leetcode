042d7515987a6b529e932fcd96fb0c8eb7340166
immark7 <immarkcuoi@gmail.com>
2024-01-18 11:38:51 +0700

    solve: suffle the array

---
+++ C#/Easy/ShuffleTheArray/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace ShuffleTheArray
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Solution solution = new Solution();
+            int[] nums = { 2, 5, 1, 3, 4, 7 };
+            int n = nums.Length / 2;
+            int[] shuffledArray = solution.Shuffle(nums, n);
+
+            Console.WriteLine("Shuffled Array:");
+            foreach (int element in shuffledArray)
+            {
+                Console.Write(element + " ");
+            }
+        }
+    }
+}
---
+++ C#/Easy/ShuffleTheArray/Solution.cs
@@ -0,0 +1,17 @@
+namespace ShuffleTheArray
+{
+    public class Solution
+    {
+        public int[] Shuffle(int[] nums, int n)
+        {
+            int[] result = new int[nums.Length];
+            for (int i = 0; i < n; i++)
+            {
+                result[2 * i] = nums[i];
+                result[2 * i + 1] = nums[i + n];
+            }
+
+            return result;
+        }
+    }
+}
