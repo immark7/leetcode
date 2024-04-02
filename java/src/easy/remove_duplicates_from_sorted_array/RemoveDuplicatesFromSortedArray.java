ef2693ba35399e0320c7e0f4c9ff8e9b19055dc8
immark7 <immarkcuoi@gmail.com>
2024-04-02 20:43:15 +0700

    chore: update solution

--- java/src/easy/remove_duplicates_from_sorted_array/RemoveDuplicatesFromSortedArray.java
+++ java/src/easy/remove_duplicates_from_sorted_array/RemoveDuplicatesFromSortedArray.java
@@ -1,6 +1,5 @@
 package easy.remove_duplicates_from_sorted_array;
 
-//Problem: https://leetcode.com/problems/remove-duplicates-from-sorted-array/
 public class RemoveDuplicatesFromSortedArray {
     public static void main(String[] args) {
         int[] nums = {1, 1, 1, 3, 3, 4, 3, 2, 4, 2};
@@ -9,13 +8,15 @@ public class RemoveDuplicatesFromSortedArray {
     }
 
     public static int removeDuplicatesFromSortedArray(int[] nums) {
-        int swap = 1;
-        for (int i = 1; i < nums.length; i++) {
-            if (nums[i] != nums[i - 1]) {
-                nums[swap] = nums[i];
-                swap++;
+        if (nums.length == 0) return 0;
+
+        int i = 0;
+        for (int j = 1; j < nums.length; j++) {
+            if (nums[i] != nums[j]) {
+                nums[i] = nums[j];
+                i++;
             }
         }
-        return swap;
+        return i + 1;
     }
 }
