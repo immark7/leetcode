d88e554002a87cf37c4c8d02c6bfeba2fe382126
immark7 <immarkcuoi@gmail.com>
2024-04-13 17:32:09 +0700

    solve: min stack

---
+++ java/src/easy/min_stack/MinStack.java
@@ -0,0 +1,35 @@
+package easy.min_stack;
+
+import java.util.Stack;
+
+class MinStack {
+    private Stack<Integer> stack;
+    private Stack<Integer> minStack;
+
+    public MinStack() {
+        stack = new Stack<>();
+        minStack = new Stack<>();
+    }
+
+    public void push(int x) {
+        stack.push(x);
+        if (minStack.isEmpty() || x <= minStack.peek()) {
+            minStack.push(x);
+        }
+    }
+
+    public void pop() {
+        int element = stack.pop();
+        if (element == minStack.peek()) {
+            minStack.pop();
+        }
+    }
+
+    public int top() {
+        return stack.peek();
+    }
+
+    public int getMin() {
+        return minStack.peek();
+    }
+}
