172bd0c2bc0a3be10f75c8872768ecceae004619
immark7 <immarkcuoi@gmail.com>
2024-04-26 23:14:25 +0700

    solve: insert into bst

--- java/src/easy/search_in_bst/TreeNode.java
+++ java/src/easy/search_in_bst/TreeNode.java
@@ -1,13 +1,13 @@
 package easy.search_in_bst;
 
 public class TreeNode {
-    int val;
-    TreeNode left;
-    TreeNode right;
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
     TreeNode() {
     }
 
-    TreeNode(int val) {
+    public TreeNode(int val) {
         this.val = val;
     }
 
---
+++ java/src/medium/insert_binary_search_tree/Solution.java
@@ -0,0 +1,34 @@
+package medium.insert_binary_search_tree;
+
+
+import easy.search_in_bst.TreeNode;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution {
+    public TreeNode insertIntoBST(TreeNode root, int val) {
+        if(root == null){
+            return new TreeNode(val);
+        }
+
+        if(val < root.val){
+            root.left = insertIntoBST(root.left, val);
+        } else {
+            root.right = insertIntoBST(root.right, val);
+        }
+        return root;
+    }
+}
