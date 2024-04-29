ea92d6384dfe4af98255667c0ce25c5140986d22
immark7 <immarkcuoi@gmail.com>
2024-04-29 20:00:26 +0700

    solve: bst right side view

---
+++ java/src/medium/binary_tree_right_side_view/Solution.java
@@ -0,0 +1,43 @@
+package medium.binary_tree_right_side_view;
+
+import easy.search_in_bst.TreeNode;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class Solution {
+    public List<Integer> rightSideView(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+
+        if (root == null) {
+            return result;
+        }
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+
+        while (!queue.isEmpty()) {
+            int levelSize = queue.size();
+
+            for (int i = 0; i < levelSize; i++) {
+                TreeNode currentNode = queue.poll();
+                if (i == levelSize - 1) {
+                    result.add(currentNode.val);
+                }
+
+                if (currentNode.left != null) {
+                    queue.add(currentNode.left);
+                }
+
+                if (currentNode.right != null) {
+                    queue.add(currentNode.right);
+                }
+            }
+
+        }
+
+        return result;
+    }
+}
