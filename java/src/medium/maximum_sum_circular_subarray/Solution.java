cca98cd80d6cf0c3168759d702d0ff4de4876885
immark7 <immarkcuoi@gmail.com>
2024-05-15 08:57:33 +0700

    solve: maximum sum circular subarray

---
+++ java/src/medium/maximum_sum_circular_subarray/Solution.java
@@ -0,0 +1,17 @@
+package medium.maximum_sum_circular_subarray;
+
+public class Solution {
+    public int maxSubarraySumCircular(int[] nums) {
+        int curMax = 0, curMin = 0;
+        int globMax = nums[0], globMin = nums[0];
+        int total = 0;
+        for (int n : nums) {
+            curMax = Math.max(curMax + n, n);
+            curMin = Math.min(curMin + n, n);
+            total += n;
+            globMax = Math.max(curMax, globMax);
+            globMin = Math.min(curMin, globMin);
+        }
+        return globMax > 0 ? Math.max(globMax, total - globMin) : globMax;
+    }
+}
