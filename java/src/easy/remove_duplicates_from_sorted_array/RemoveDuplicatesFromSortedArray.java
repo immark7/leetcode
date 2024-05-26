3693e2263de05121031b60a3d841ccb2b6ad0aba
immark7 <immarkcuoi@gmail.com>
2024-05-26 14:51:02 +0700

    refactor: duplicate probile

--- java/src/easy/remove_duplicates_from_sorted_array/RemoveDuplicatesFromSortedArray.java
+++ java/src/easy/remove_duplicates_from_sorted_array/RemoveDuplicatesFromSortedArray.java
@@ -1,5 +1,6 @@
 package easy.remove_duplicates_from_sorted_array;
 
+// Two pointer
 public class RemoveDuplicatesFromSortedArray {
     public static void main(String[] args) {
         int[] nums = {1, 1, 1, 3, 3, 4, 3, 2, 4, 2};
@@ -10,13 +11,16 @@ public class RemoveDuplicatesFromSortedArray {
     public static int removeDuplicatesFromSortedArray(int[] nums) {
         if (nums.length == 0) return 0;
 
-        int i = 0;
-        for (int j = 1; j < nums.length; j++) {
-            if (nums[i] != nums[j]) {
-                i++;
-                nums[i] = nums[j];
+
+        // pointer for the unique element
+        int k = 1;
+
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] != nums[i - 1]) {
+                nums[k] = nums[i];
+                k++;
             }
         }
-        return i + 1;
+        return k;
     }
 }
