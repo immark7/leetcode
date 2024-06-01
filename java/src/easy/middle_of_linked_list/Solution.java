eb93ea2449d46c1717490ffc05a81a4180a5b776
immark7 <immarkcuoi@gmail.com>
2024-06-01 21:42:56 +0700

    solve: middle linkedlist

---
+++ java/src/easy/middle_of_linked_list/Solution.java
@@ -0,0 +1,23 @@
+package easy.middle_of_linked_list;
+
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int val) {
+        this.val = val;
+    }
+}
+
+class Solution {
+    public ListNode middleNode(ListNode head){
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while(fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        return slow;
+    }
+}
