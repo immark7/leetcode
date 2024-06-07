c6b8979d13f9a0289ad8b658c32c584ee8faee48
immark7 <immarkcuoi@gmail.com>
2024-06-08 01:27:46 +0700

    solve: implement trie

---
+++ java/src/medium/implement_trie/Solution.java
@@ -0,0 +1,55 @@
+package medium.implement_trie;
+
+class TrieNode {
+    TrieNode[] children;
+    boolean isEndOfWord;
+
+    public TrieNode() {
+        children = new TrieNode[26];
+        isEndOfWord = false;
+    }
+}
+
+class Trie {
+    private TrieNode root;
+
+    public Trie() {
+        root = new TrieNode();
+    }
+
+    public void insert(String word) {
+        TrieNode currentNode = root;
+        for (char c : word.toCharArray()) {
+            int index = c - 'a';
+            if (currentNode.children[index] == null) {
+                currentNode.children[index] = new TrieNode();
+            }
+            currentNode = currentNode.children[index];
+        }
+        currentNode.isEndOfWord = true;
+    }
+
+    public boolean search(String word) {
+        TrieNode currentNode = root;
+        for (char c : word.toCharArray()) {
+            int index = c - 'a';
+            if (currentNode.children[index] == null) {
+                return false;
+            }
+            currentNode = currentNode.children[index];
+        }
+        return currentNode.isEndOfWord;
+    }
+
+    public boolean startsWith(String prefix) {
+        TrieNode currentNode = root;
+        for (char c : prefix.toCharArray()) {
+            int index = c - 'a';
+            if (currentNode.children[index] == null) {
+                return false;
+            }
+            currentNode = currentNode.children[index];
+        }
+        return true;
+    }
+}
