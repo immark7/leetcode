6244f6b819be81f95cd63b3f8c9d72d03d3d3731
immark7 <immarkcuoi@gmail.com>
2024-06-21 22:51:50 +0700

    feat: solve min stack

---
+++ java/src/medium/min_stack/Solution.java
@@ -0,0 +1,35 @@
+package medium.min_stack;
+
+import java.util.Stack;
+
+public class Solution {
+    private Stack<Integer> stack;
+    private Stack<Integer> minStack;
+
+    public Solution() {
+        stack = new Stack<>();
+        minStack = new Stack<>();
+    }
+
+    public void push(int x) {
+        stack.push(x);
+        if (minStack.isEmpty() || x <= minStack.peek()) {
+            minStack.push(x);
+        }
+    }
+
+    public void pop() {
+        int element = stack.pop();
+        if (element == minStack.peek()) {
+            minStack.pop();
+        }
+    }
+
+    public int top() {
+        return stack.peek();
+    }
+
+    public int getMin() {
+        return minStack.peek();
+    }
+}
