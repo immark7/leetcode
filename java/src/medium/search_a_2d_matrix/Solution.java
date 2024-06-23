0d56e2986c201ebae708772970987ccaa2435969
immark7 <immarkcuoi@gmail.com>
2024-06-23 17:12:36 +0700

    solve: search in a 2d matrix

---
+++ java/src/medium/search_a_2d_matrix/Solution.java
@@ -0,0 +1,28 @@
+package medium.search_a_2d_matrix;
+
+public class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
+            return false;
+        }
+
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+        int left = 0;
+        int right = rows * cols - 1;
+
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            int midValue = matrix[mid / cols][mid % cols];
+            if (midValue == target) {
+                return true;
+            } else if (midValue < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return false;
+    }
+}
