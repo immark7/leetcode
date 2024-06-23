02ca527a29f258d49bcf4d5265dab38c61fbedf2
immark7 <immarkcuoi@gmail.com>
2024-06-23 21:41:12 +0700

    solve: best time to buy and sell stock

---
+++ java/src/easy/best_time_to_buy_and_sell_stock/Solution.java
@@ -0,0 +1,26 @@
+package easy.best_time_to_buy_and_sell_stock;
+
+public class Solution {
+    public int maxProfit(int[] prices) {
+        if (prices == null || prices.length < 2) {
+            return 0;
+        }
+
+        int left = 0;
+        int right = 1;
+        int maxProfit = 0;
+
+        while (right < prices.length) {
+            if (prices[left] < prices[right]) {
+                int currentProfit = prices[right] - prices[left];
+                maxProfit = Math.max(maxProfit, currentProfit);
+            } else {
+                left = right;
+            }
+
+            right++;
+        }
+
+        return maxProfit;
+    }
+}
