821c4e0ca68dda751bbd4f78e5cb332642d29368
immark7 <immarkcuoi@gmail.com>
2024-06-28 22:54:59 +0700

    solve: product of array except self

---
+++ java/src/medium/product_of_array_except_self/Solution.java
@@ -0,0 +1,23 @@
+package medium.product_of_array_except_self;
+
+public class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        int[] result = new int[nums.length];
+        int left = 1;
+        int right = 1;
+
+        // Left product
+        for (int i = 0; i < nums.length; i++) {
+            result[i] = left;
+            left *= nums[i];
+        }
+
+        // Right product
+        for (int i = nums.length - 1; i >= 0; i--) {
+            result[i] *= right;
+            right *= nums[i];
+        }
+
+        return result;
+    }
+}
