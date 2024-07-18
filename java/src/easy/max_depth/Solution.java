7489d0e00eef06c13f6f2ce28cead4a19c9b440c
immark7 <immarkcuoi@gmail.com>
2024-07-19 01:11:51 +0700

    solve: max depth bst

---
+++ java/src/easy/max_depth/Solution.java
@@ -0,0 +1,14 @@
+package easy.max_depth;
+
+import easy.search_in_bst.TreeNode;
+
+public class Solution {
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int leftDepth = maxDepth(root.left);
+        int rightDepth = maxDepth(root.right);
+        return Math.max(leftDepth, rightDepth) + 1;
+    }
+}
