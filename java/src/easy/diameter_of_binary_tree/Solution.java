9c96c9d2b16ab7091e621f138173f323a4e41bf5
immark7 <immarkcuoi@gmail.com>
2024-07-21 13:06:02 +0700

    solve: diameter of binary tree

---
+++ java/src/easy/diameter_of_binary_tree/Solution.java
@@ -0,0 +1,25 @@
+package easy.diameter_of_binary_tree;
+
+import easy.search_in_bst.TreeNode;
+
+class Solution {
+    private int diameter = 0;
+
+    public int diameterOfBinaryTree(TreeNode root) {
+        dfs(root);
+        return diameter;
+    }
+
+    private int dfs(TreeNode node) {
+        if (node == null) {
+            return 0;
+        }
+
+        int leftDepth = dfs(node.left);
+        int rightDepth = dfs(node.right);
+
+        diameter = Math.max(diameter, leftDepth + rightDepth);
+
+        return Math.max(leftDepth, rightDepth) + 1;
+    }
+}
