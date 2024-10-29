b47137a331f75dacf85f1f69a72fa7ec702ccb89
immark7 <immarkcuoi@gmail.com>
2024-10-29 21:00:06 +0700

    solve: rotate array

---
+++ java/src/medium/rotate_array/Solution.java
@@ -0,0 +1,24 @@
+package medium.rotate_array;
+
+public class Solution {
+    public void rotate(int[] nums, int k) {
+        k = k % nums.length;
+
+        // Reverse the entire array
+        reverse(nums, 0, nums.length - 1);
+        // Reverse first k elements
+        reverse(nums, 0, k - 1);
+        // Reverse remaining elements
+        reverse(nums, k, nums.length - 1);
+    }
+
+    private void reverse(int[] nums, int start, int end) {
+        while (start <= end) {
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start++;
+            end--;
+        }
+    }
+}
