f60fdf06a02d8c959de287e7ccd91e4537d846a7
immark7 <immarkcuoi@gmail.com>
2024-11-28 00:57:24 +0700

    chore: invert tree using bfs

--- java/src/easy/invert_tree/Solution.java
+++ java/src/easy/invert_tree/Solution.java
@@ -2,6 +2,9 @@ package easy.invert_tree;
 
 import easy.search_in_bst.TreeNode;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 class Solution {
     public TreeNode invertTree(TreeNode root) {
         if (root == null) {
@@ -14,6 +17,33 @@ class Solution {
 
         invertTree(root.left);
         invertTree(root.right);
+
+        return root;
+    }
+
+    public TreeNode invertTreeBFS(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+
+        while (!queue.isEmpty()) {
+            TreeNode node = queue.poll();
+
+            TreeNode temp = node.left;
+            node.left = node.right;
+            node.right = temp;
+
+            if (node.left != null) {
+                queue.offer(node.left);
+            }
+
+            if (node.right != null) {
+                queue.offer(node.right);
+            }
+        }
 
         return root;
     }
