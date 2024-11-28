4eb418e0538d3e79f9bb05e0ec621b2e100d7e5c
immark7 <immarkcuoi@gmail.com>
2024-11-28 09:39:01 +0700

    chore: max depth of tree using bfs

--- java/src/easy/max_depth/Solution.java
+++ java/src/easy/max_depth/Solution.java
@@ -2,6 +2,9 @@ package easy.max_depth;
 
 import easy.search_in_bst.TreeNode;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class Solution {
     public int maxDepth(TreeNode root) {
         if (root == null) {
@@ -11,4 +14,32 @@ public class Solution {
         int rightDepth = maxDepth(root.right);
         return Math.max(leftDepth, rightDepth) + 1;
     }
+
+    public int maxDepthBFS(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+
+        int depth = 0;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+
+        while (!queue.isEmpty()) {
+            int levelSize = queue.size();
+
+            for (int i = 0; i < levelSize; i++) {
+                TreeNode node = queue.poll();
+                if (node.left != null) {
+                    queue.offer(node.left);
+                }
+
+                if (node.right != null) {
+                    queue.offer(node.right);
+                }
+            }
+        }
+
+        return depth;
+    }
+
 }
